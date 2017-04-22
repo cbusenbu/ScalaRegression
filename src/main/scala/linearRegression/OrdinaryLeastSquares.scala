b74681a291218fcfc3f9bb4b66c6ba2f047af43f
Chas Busenburg <cbusenbu@gmail.com>
2017-04-22 08:39:22 -0500

    add predict function back into OrdinaryLeastSquares.scala

--- src/main/scala/linearRegression/OrdinaryLeastSquares.scala
+++ src/main/scala/linearRegression/OrdinaryLeastSquares.scala
@@ -6,50 +6,62 @@ import breeze.linalg._
 
 
 
-class OrdinaryLeastSquares(val olsData:DenseMatrix[Double],var predictColumn:Int) extends linearRegression {
-  var wLimMatrix:DenseMatrix[Double] = DenseMatrix.zeros[Double](1,1)
+class OrdinaryLeastSquares(trainingData:NormalizedData[Double]],
+                          predictColumn:Int,
+                          bFunction:BasisFunction ) extends linearRegression {
 
-  def setPredictColumn(columnNum:Int):Unit ={
-    this.predictColumn = columnNum
-    wLimMatrix = DenseMatrix.zeros[Double](1,1)
+  private var wlimvector =densevector.fill(1){0.0}
+
+  def setpredictcolumn(columnnum:int):unit ={
+    this.predictcolumn = columnnum
+    wlimmatrix = densematrix.fill(1){0.0}
   }
 
-  private def predictVector: DenseVector[Double] = {
-    olsData(::,predictColumn)
+  private def predictvector: densevector[double] = {
+    olsdata(::,predictcolumn)
   }
-  private def lengthPV: Integer = predictVector.length
+  private def lengthpv: integer = predictvector.length
 
-  private def columnOnes:DenseMatrix[Double] = DenseMatrix.zeros[Double](lengthPV,1) map {case x => x+1}
+  private def columnones:densematrix[double] = densematrix.zeros[double](lengthpv,1) map {case x => x+1}
 
-  private def olsDataNotPredictColumn:DenseMatrix[Double] = {
-    val copyOLSData = olsData
-    copyOLSData.delete(predictColumn,Axis._1)
+  private def olsdatanotpredictcolumn:densematrix[double] = {
+    val copyolsdata = olsdata
+    copyolsdata.delete(predictcolumn,axis._1)
   }
-  private def together:DenseMatrix[Double] = DenseMatrix.horzcat(columnOnes,olsDataNotPredictColumn)
+  private def together:densematrix[double] = densematrix.horzcat(columnones,olsdatanotpredictcolumn)
 
-  private def pInverse:DenseMatrix[Double] = pinv(together)
+  private def pinverse:densematrix[double] = pinv(together)
 
-  private def prepPredictData(predictData:DenseVector[Double]): DenseVector[Double] = {
-    val leftVector:DenseVector[Double] = predictData.slice(0,predictColumn)
-    val rightVector:DenseVector[Double] = predictData.slice(predictColumn+1,predictData.length)
-    val returnVector = DenseVector.vertcat(leftVector,rightVector)
-    DenseVector.vertcat(DenseVector.ones[Double](1),returnVector)
+  private def preppredictdata(predictdata:densevector[double]): densevector[double] = {
+    val leftvector:densevector[double] = predictdata.slice(0,predictcolumn)
+    val rightvector:densevector[double] = predictdata.slice(predictcolumn+1,predictdata.length)
+    val returnvector = densevector.vertcat(leftvector,rightvector)
+    densevector.vertcat(densevector.ones[double](1),returnvector)
   }
 
-  def wlim:DenseVector[Double] = {
-    wLimMatrix = pInverse*predictVector.asDenseMatrix.t
-    wLimMatrix.toDenseVector
+  def wlim:densevector[double] = {
+    wlimmatrix = pinverse*predictvector.asdensematrix.t
+    wlimmatrix.todensevector
   }
-  def predict(testData:DenseMatrix[Double]):DenseVector[Double] ={
-    return testData.toDenseVector
+  def predict(testData:densematrix[double]):densevector[double] ={
+    val predictions = DenseVector.fill(testData.rows){0.0}
+    if (wlimvector == densevector.fill(1){0.0}){
+      throw new wlimnotintializedexception("wlim not initialized, recommend running olsobject.wlim")
+    }
+    for (rowNum <- 0 to testdata.rows ){
+      val predictedData:DenseVector[Double] = preppredictdata(testData(rowNum,::))
+      predictions(rowNum) = predicteddata.asdensematrix*wlimmatrix
+    }
+    predictions 
   }
   /*
-  def predict(predictData:DenseVector[Double]):DenseMatrix[Double] = {
-    if (wLimMatrix == DenseMatrix.zeros[Double](1,1)){
-      throw new WLimNotIntializedException("wLim Not Intialized, recommend running OLSObject.wlim")
+  def predict(predictdata:densevector[double]):densematrix[double] = {
+    
+    if (wlimvector == densevector.fill(1){0.0}){
+      throw new wlimnotintializedexception("wlim not intialized, recommend running .wlim")
     }
-    val predictedData:DenseVector[Double] = prepPredictData(predictData)
-    predictedData.asDenseMatrix*wLimMatrix
+    val predicteddata:densevector[double] = preppredictdata(predictdata)
+    predicteddata.asdensematrix*wlimmatrix
   }
   */
 }
